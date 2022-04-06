9bdaa56c0b706fa173771bdd364a416d0394ae3d
arturlector <art.igberdin@gmail.com>
2022-04-06 19:43:38 +0300

    feat(app): Initialize project

---
+++ vk-ilia/Screens/AuthViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vk-ilia
+//
+//  Created by Artur Igberdin on 06.04.2022.
+//
+
+import UIKit
+
+class AuthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+}
+
